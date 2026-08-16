440400de6890ee250a69bd3d12370279830f84fb
Vojtěch Faltýnek <faltynekvojtech@gmail.com>
2026-08-17 01:47:00 +0200

    margin top

--- new-frontend/src/app/o-nas/page.tsx
+++ new-frontend/src/app/o-nas/page.tsx
@@ -2,7 +2,7 @@ import { About } from "@/components/home/About";
 
 export default function AboutPage() {
   return (
-    <div className="flex flex-col min-h-screen pt-32">
+    <div className="flex flex-col min-h-screen">
       <main className="flex-1">
         <About />
       </main>
